dda0e634c572738a37e8d6035be2dce3389e3a9c
MarianoMarquesSerra <marianomarquesserra@gmail.com>
2023-02-06 17:28:39 -0300

    Pagina de Ayuda finalizada

--- src/componentes/MenuVentas.js
+++ src/componentes/MenuVentas.js
@@ -7,26 +7,26 @@ const MenuVentas = () => {
     return ( 
         <>
             <div class="container"  style={{position: 'absolute', top: '225px', left: '115px'}}>
-                <div class="row justify-content-center align-items-center  gap-4">
-                    <div class="col-2 card justify-content-center align-items-center" style={{height: '180px', width: '180px', cursor: 'pointer'}}>
+                <div class="row justify-content-center align-items-center gap-4">
+                    <div class="col-2 card justify-content-center align-items-center addShade" style={{height: '180px', width: '180px', cursor: 'pointer'}}>
                             <img class="card-img-top w-50" src={imgProductos} style={{position: 'relative', top: '50px'}}  alt="Card image cap"/>
                       <div class="card-body" style={{position: 'relative', top: '50px'}} >
                         <h5 class="card-title">Productos</h5>
                       </div>
                     </div>
-                    <div class="col-2 card justify-content-center align-items-center" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
+                    <div class="col-2 card justify-content-center align-items-center addShade" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
                       <img class="card-img-top w-50" src={imgVehiculos} style={{position: 'relative', top: '30px'}}  alt="Card image cap"/>
                       <div class="card-body" style={{position: 'relative', top: '20px'}} >
                         <h5 class="card-title">Vehículos</h5>
                       </div>
                     </div>
-                    <div class="col-2 card justify-content-center align-items-center" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
+                    <div class="col-2 card justify-content-center align-items-center addShade" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
                       <img class="card-img-top w-50" src={imgInmuebles} style={{position: 'relative', top: '10px'}} alt="Card image cap"/>
                       <div class="card-body" >
                         <h5 class="card-title">Inmuebles</h5>
                       </div>
                     </div>
-                    <div class="col-2 card justify-content-center align-items-center" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
+                    <div class="col-2 card justify-content-center align-items-center addShade" style={{height: '180px', width: '180px' , cursor: 'pointer'}}>
                       <img class="card-img-top w-50" src={imgServicios} style={{position: 'relative', top: '30px'}} alt="Card image cap"/>
                       <div class="card-body" style={{position: 'relative', top: '30px'}}>
                         <h5 class="card-title">Servicios</h5>
--- src/paginas/Ayuda.js
+++ src/paginas/Ayuda.js
@@ -1,10 +1,12 @@
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { BiChevronRight } from "react-icons/bi";
 import { NavLink } from "react-router-dom"; 
+import FooterProductos from "../componentes/FooterProductos";
 import Navbar from "../componentes/Navbar";
 import	iconoCompras from "../images/iconoCompras.jpg";
 import iconoDevoluciones from "../images/iconoDevoluciones.jpg";
 import iconoPreguntas from "../images/iconoPreguntas.jpg";
+import iconoVentas from "../images/iconoVentas.jpg";
 
 const Ayuda = () => {
     return ( 
@@ -28,33 +30,204 @@ const Ayuda = () => {
             </div>
             <div className="row" style={{position:'relative',top:'80px',left:'285px'}}>
                 <div className="col-7">
-                <label for="staticEmail2" style={{fontWeight: 'bold', fontSize: '20px'}}>Compras</label>
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey'}}>Compras</label>
+                </div>
                 <ul class="list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <span><img src={iconoCompras}/></span>
-                        <div>
-                            <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Administrar y cancelar compras</span><br/>
-                            <span class="andes-list__item-secondary">Pagar, seguir envíos, modificar, reclamar o cancelar compras.</span>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelectedBorder">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-1" style={{position:'relative',top: '25%'}}>
+                                <span><img src={iconoCompras}/></span>
+                            </div>
+                            <div className="col-10" style={{position: 'relative', top:'10%'}}>
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Administrar y cancelar compras</span><br/>
+                                <span class="andes-list__item-secondary" style={{fontSize: '14px',color:'gray'}}>Pagar, seguir envíos, modificar, reclamar o cancelar compras.</span>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
                         </div>
-                        <span><BiChevronRight/></span>
                     </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <span><img src={iconoDevoluciones}/></span>
-                        <div>
-                            <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Devoluciones y reembolsos</span><br/>
-                            <span class="andes-list__item-secondary">Devolver un producto o consultar por reintegros de dinero de una compra.</span>
-                        </div>
-                      <span><BiChevronRight/></span>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelectedBorder">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-1"  style={{position:'relative',top: '25%'}}>
+                                <span><img src={iconoDevoluciones}/></span>
+                            </div>
+                            <div className="col-10" style={{position: 'relative', top:'10%'}}>
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Devoluciones y reembolsos</span><br/>
+                                <span class="andes-list__item-secondary" style={{fontSize: '14px',color:'gray'}}>Devolver un producto o consultar por reintegros de dinero de una compra.</span>                    
+                            </div>
+                            <div className="col-1"  style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>
+                            </div>
+                        </div>     
                     </li>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <span><img src={iconoPreguntas}/></span>
-                        <div>
-                            <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Preguntas frecuentes sobre compras</span><br/>
-                        </div>
-                      <span><BiChevronRight/></span>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelectedBorder">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-1"  style={{position:'relative',top: '25%'}}>
+                                <span><img src={iconoPreguntas}/></span>
+                            </div>
+                            <div className="col-10" style={{position: 'relative', top:'30%'}}>
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Preguntas frecuentes sobre compras</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>     
+                            </div>
+                        </div>                
                     </li>
                 </ul>
                 </div>
+            </div>
+
+            <div className="row" style={{position:'relative',top:'140px',left:'285px'}}>
+                <div className="col-7">
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey',height:'10px'}}>Ventas</label>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelectedBorder">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-1" style={{position:'relative',top: '25%'}}>
+                                <span><img src={iconoVentas}/></span>
+                            </div>
+                            <div className="col-10" style={{position: 'relative', top:'10%'}}>
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Gestionar ventas y publicaciones</span><br/>
+                                <span class="andes-list__item-secondary" style={{fontSize: '14px',color:'gray'}}>Ventas, cobros, envios, reclamos, devoluciones, publicaciones y reputación</span>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelectedBorder">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-1"  style={{position:'relative',top: '25%'}}>
+                                <span><img src={iconoPreguntas}/></span>
+                            </div>
+                            <div className="col-10" style={{position: 'relative', top:'30%'}}>
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold'}}>Preguntas frecuentes sobre ventas</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>     
+                            </div>
+                        </div>                
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div className="row" style={{position:'relative',top:'180px',left:'285px'}}>
+                <div className="col-7">
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey',height:'10px'}}>Ayuda sobre tu cuenta</label>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Configuración de mi cuenta</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Seguridad</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>     
+                            </div>
+                        </div>                
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div className="row" style={{position:'relative',top:'220px',left:'285px'}}>
+                <div className="col-7">
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey',height:'10px'}}>Estos temas pueden interesarte</label>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Políticas de devolución de tus compras</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Cómo devuelvo un producto</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>     
+                            </div>
+                        </div>                
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Cómo cancelo una compra</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>     
+                            </div>
+                        </div>                
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div className="row" style={{position:'relative',top:'260px',left:'285px'}}>
+                <div className="col-7">
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey',height:'10px'}}>Tus últimas consultas</label>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Consultas</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div className="row" style={{position:'relative',top:'300px',left:'285px'}}>
+                <div className="col-7">
+                <div style={{height:'10px'}}>
+                    <label for="staticEmail2" style={{fontSize: '20px',color:'grey',height:'10px'}}>¿Necesitas más ayuda?</label>
+                </div>
+                <ul class="list-group">
+                    <li class="list-group-item d-flex justify-content-between align-items-center itemSelected">
+                        <div className="row col-12" style={{height:'60px'}}>
+                            <div className="col-11">
+                                <span class="andes-list__item-primary" style={{fontWeight: 'bold', position: 'relative', top:'30%'}}>Contactanos</span><br/>
+                            </div>
+                            <div className="col-1" style={{position:'relative',top: '20%',fontSize:'25px',color:'gray'}}>
+                                <span><BiChevronRight/></span>        
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+            <div style={{position: 'relative', top: '400px'}}>
+                <FooterProductos/>
             </div>
         </>
      );
